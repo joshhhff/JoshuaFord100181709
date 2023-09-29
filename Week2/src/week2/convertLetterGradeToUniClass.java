1be4f51c5b28619e6e2e9e352a407dbc122902e1
joshhhff <100181709@student.burnley.ac.uk>
2023-09-29 11:03:52 +0100

    Added validation to user input

--- Week2/src/week2/convertLetterGradeToUniClass.java
+++ Week2/src/week2/convertLetterGradeToUniClass.java
@@ -7,18 +7,18 @@ public class convertLetterGradeToUniClass {
 
         while(true){
 
-        System.out.println("Enter letter grade to convert to university classification:");
-        String grade = in.next().toUpperCase();
+            System.out.println("Enter letter grade to convert to university classification:");
+            String grade = in.next().toUpperCase();
 
-        if (grade.equals("A")){System.out.println("Your university equivalent grade is: 1st");}
-        else if (grade.equals("B")){System.out.println("Your university equivalent grade is: 2:1");}
-        else if (grade.equals("C")){System.out.println("Your university equivalent grade is: 2:2");}
-        else if (grade.equals("D")){System.out.println("Your university equivalent grade is: 3rd");}
-        else if (grade.equals("E")){System.out.println("Your university equivalent grade is: Ordinary");}
-        else if (grade.equals("F")){System.out.println("Your university equivalent grade is: Fail");}
-        else{
-            grade=in.next();
-        }
+            if (grade.equals("A")){System.out.println("Your university equivalent grade is: 1st"); break;}
+            else if (grade.equals("B")){System.out.println("Your university equivalent grade is: 2:1"); break;}
+            else if (grade.equals("C")){System.out.println("Your university equivalent grade is: 2:2"); break;}
+            else if (grade.equals("D")){System.out.println("Your university equivalent grade is: 3rd"); break;}
+            else if (grade.equals("E")){System.out.println("Your university equivalent grade is: Ordinary"); break;}
+            else if (grade.equals("F")){System.out.println("Your university equivalent grade is: Fail"); break;}
+            else{
+                System.out.println("Error: Please try again");
+            }
         }
 
     }
