4c3e50917b07f92bac40a3592b1e97c11091c373
joshhhff <100181709@student.burnley.ac.uk>
2023-09-29 10:14:47 +0100

    Commit

---
+++ Week2/src/CalcIf18.java
@@ -0,0 +1,71 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class CalcIf18 {
+
+public static void main(String[]args){
+    Scanner in = new Scanner(System.in);
+
+    int birthYear, birthMonth, birthDay;
+
+    while (true) {
+        System.out.println("Enter your birth year:");
+        if (!in.hasNextInt()) { // If the next input from the Scanner object "in" is not an integer
+            System.out.println("Error: Enter a number");
+            in.next();  // Discard last input and try again
+        } else {
+            birthYear = in.nextInt();
+            // Check if the birth year is a 4-digit number
+            if (birthYear < 1000 || birthYear > 9999) {
+                System.out.println("Error: Enter a 4-digit number");
+            } else {
+                break;
+            }
+        }
+    }
+
+    while (true) {
+        System.out.println("Enter your birth month:");
+        if (!in.hasNextInt()) { //If the next input from the Scanner object "in" is not an integer
+            System.out.println("Error: Enter a number");
+            in.next();  //Discard last input and try again
+        } else {
+            birthMonth = in.nextInt();
+
+            if (birthMonth>=1 && birthMonth<=12){
+                break;
+            } else{
+                System.out.println("Enter a valid number for month. (1-12)");
+                in.next();
+            }
+        }
+    }
+
+    while (true) {
+        System.out.println("Enter your birth day:");
+        if (!in.hasNextInt()) { //If the next input from the Scanner object "in" is not an integer
+            System.out.println("Error: Enter a number");
+            in.next();  //Discard last input and try again
+        } else {
+            birthDay = in.nextInt();
+
+            if (birthDay>=1 && birthDay<=31){
+                break;
+            } else{
+                System.out.println("Enter a valid number for days of the month. (1-31)");
+                in.next();
+            }
+            break;
+        }
+    }
+
+    LocalDate today = LocalDate.now();
+    LocalDate birthDate = LocalDate.of(birthYear, birthMonth, birthDay);
+
+    Period period = Period.between(birthDate, today);
+    long age = period.getYears();
+
+    System.out.println("You are " + age + " years old.");
+}
+}
