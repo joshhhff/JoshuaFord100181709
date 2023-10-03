a96e5297301c6f4f156f04ae8d060d085b8988e4
joshhhff <100181709@student.burnley.ac.uk>
2023-10-03 13:54:15 +0100

    Efficiently loop through the seven times table

---
+++ Week2/src/week2/SevenTimesTable.java
@@ -0,0 +1,10 @@
+package week2;
+
+public class SevenTimesTable {
+    public static void main(String[] args){
+
+        for(int i=1; i<13; i++){
+            System.out.println(i*7);
+        }
+    }
+}
