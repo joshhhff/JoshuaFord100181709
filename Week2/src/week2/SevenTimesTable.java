8035b225f5a14aaa8da4ea8e5e799f54bc6ec939
joshhhff <100181709@student.burnley.ac.uk>
2023-10-03 13:57:18 +0100

    User can choose what multiplications they want to see

--- Week2/src/week2/SevenTimesTable.java
+++ Week2/src/week2/SevenTimesTable.java
@@ -1,10 +1,25 @@
 package week2;
 
+import java.util.Scanner;
+
 public class SevenTimesTable {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int user;
+        while (true) {
+            System.out.println("Enter whole number:");
+            if (!in.hasNextInt()) { // If the next input from the Scanner object "in" is not an integer
+                System.out.println("Error: Enter a number");
+                in.next();  // Discard last input and try again
+            } else {
+                user = in.nextInt();
+                break;
+                }
+            }
 
         for(int i=1; i<13; i++){
-            System.out.println(i*7);
+            System.out.println(i*user);
         }
     }
 }
