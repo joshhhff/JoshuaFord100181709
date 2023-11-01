5950c3a0a3a8a44fca6f07dedf583e55600cecc6
joshhhff <100181709@student.burnley.ac.uk>
2023-11-01 13:24:20 +0000

    Assignment 1 script changes

---
+++ MusicPlayer/src/MusicPlayer/Song.java
@@ -0,0 +1,33 @@
+package MusicPlayer;
+
+public class Song {
+    private String songName;
+    private String artistName;
+    private int playCount;
+
+    public Song(String artistName, String songName){
+        this.artistName = artistName;
+        this.songName = songName;
+        this.playCount = 0;
+    }
+
+    public String getArtistName(){
+        return artistName;
+    }
+
+    public String getSongName(){
+        return songName;
+    }
+
+    public int getPlayCount(){
+        return playCount;
+    }
+
+    public void setPlayCount(){
+        playCount++;
+    }
+
+    public String toString(){
+        return artistName + "-" + songName + "(Number of plays: " + playCount + ")";
+    }
+}
